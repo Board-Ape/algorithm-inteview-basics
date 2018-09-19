90933a3e074e1f96bbaca9d093d19e54a171401f
Sam Singer <samuel.singer1@gmail.com>
2018-09-18 23:29:46 -0700

    Midpoint to a linkedlist can be found utilizing two variables to track which point you are at

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,18 +12,32 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
+
 function midpoint(list) {
-
     let slow = list.getFirst();
-    let fast = list.getFirst();
-
+    let fast = list.head;
 
     while (fast.next && fast.next.next) {
         slow = slow.next;
-        fast = fast.next.next;
+        fast = fast.next.next
     }
 
     return slow;
 }
+
+
+// function midpoint(list) {
+
+//     let slow = list.getFirst();
+//     let fast = list.getFirst();
+
+
+//     while (fast.next && fast.next.next) {
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }
+
+//     return slow;
+// }
 
 module.exports = midpoint;
