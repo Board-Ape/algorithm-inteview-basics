44e519ddb74705d5463f7c498acff63a30619927
Sam Singer <samuel.singer1@gmail.com>
2018-09-21 12:33:30 -0700

    Traverse Breadth First

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -49,9 +49,17 @@ class Tree {
        }
     }
 
-    traverseDF() {
+    traverseDF(fn) {
+        const arr = [this.root];
 
-    }
+        while (arr.length) {
+            const node = arr.shift();
+
+            arr.unshift(...node.children);
+            fn(node);
+        }
+
+     }
 }
 
 module.exports = { Tree, Node };
