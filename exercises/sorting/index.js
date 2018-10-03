b2e3de344702634c8da9f87ff271a78f05f05def
Sam Singer <samuel.singer1@gmail.com>
2018-10-02 18:37:59 -0700

    Selection sort is complete

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,20 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        let temp;
+        for (let j = 1 + i; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
 
+    return arr;
 }
 
 function mergeSort(arr) {
