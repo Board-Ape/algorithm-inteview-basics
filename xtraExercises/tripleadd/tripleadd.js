8ddc50ee09ad88bc5d4f21a9cb22b85494a09d08
Sam Singer <samuel.singer1@gmail.com>
2018-10-15 13:39:47 -0700

    Two solutions found

--- xtraExercises/tripleadd/tripleadd.js
+++ xtraExercises/tripleadd/tripleadd.js
@@ -1,3 +1,14 @@
 tripleAdd(10)(20)(30);
 // returns total of all 3 numbers added together 
 
+const tripleAdd = (num1) => (num2) => (num3) => num1 + num2 + num3
+
+function tripleAdd(num1) {
+    return function (num2) {
+        return function (num3) {
+            return num1 + num2 + num3
+        }
+    }
+}
+
+tripleAdd(10)(20)(30)
