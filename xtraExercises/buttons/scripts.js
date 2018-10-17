a90d50dc73bb575f9cac8b5642451ebd1b8d0755
Sam Singer <samuel.singer1@gmail.com>
2018-10-17 16:55:47 -0700

    Create a separate function call

--- xtraExercises/buttons/scripts.js
+++ xtraExercises/buttons/scripts.js
@@ -64,3 +64,27 @@ function createButtons() {
 }
 
 createButtons();
+
+/*
+    Use a seperate function call!
+
+    Allowing the i to now be passed into a separate function
+*/
+
+function createButtons() {
+    for (var i = 1; i <= 5; i++) {
+        var body = document.getElementsByTagName("BODY")[0];
+        var button = document.createElement("BUTTON");
+        button.innerHTML = 'Button ' + i;
+        addClickFunctionality(button, i)
+        body.appendChild(button);
+    }
+}
+
+createButtons();
+
+function addClickFunctionality(button, num) {
+    button.onclick = function () {
+        alert('This is button ' + num);
+    }
+}
