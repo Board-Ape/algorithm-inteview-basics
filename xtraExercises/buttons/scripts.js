aa8f099bfaf9ca7355363aa83cb992de8b21279a
Sam Singer <samuel.singer1@gmail.com>
2018-10-17 16:46:06 -0700

    This swill output the button correctly

--- xtraExercises/buttons/scripts.js
+++ xtraExercises/buttons/scripts.js
@@ -28,3 +28,19 @@ createButtons();
 
 */
 
+function createButtonsIIFE() {
+
+    for (var i = 1; i <= 5; i++) {
+        var body = document.getElementsByTagName("BODY")[0];
+        var button = document.createElement("BUTTON");
+        button.innerHTML = 'Button ' + i;
+        (function (num) {
+            button.onclick = function () {
+                alert('This is button ' + num);
+            }
+        })(i);
+        body.appendChild(button);
+    }
+}
+
+createButtonsIIFE()
