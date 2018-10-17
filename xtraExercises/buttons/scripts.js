25377101ea040c1a281a5f875c1b0b41841f4fc5
Sam Singer <samuel.singer1@gmail.com>
2018-10-17 16:45:48 -0700

    Button 5 will alert the user, This is button 6

---
+++ xtraExercises/buttons/scripts.js
@@ -0,0 +1,30 @@
+function createButtons() {
+    for (var i = 1; i <= 5; i++) {
+        var body = document.getElementsByTagName("BODY")[0];
+        var button = document.createElement("BUTTON");
+        button.innerHTML = 'Button ' + i;
+        button.onclick = function () {
+            alert('This is button ' + i);
+        }
+        body.appendChild(button);
+    }
+}
+
+createButtons();
+
+/* 
+    What is the output of this createButtons function?
+
+    This adds 5 buttons to the screen 
+    When a button is clicked with the above code than the output will be 6...
+
+    WHY?
+
+    Loop finishes runnning for all 5 buttons, i is not left at 6 since it fails at 6. i remains equal to 6 and that's what is returned.
+
+    FIX?
+
+    Use an IIFE! 
+
+*/
+
