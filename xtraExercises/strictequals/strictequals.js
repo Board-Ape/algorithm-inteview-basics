d6f7304cc5d7645e00f30d69afaf33c43745fd7e
Sam Singer <samuel.singer1@gmail.com>
2018-10-24 18:45:06 -0700

    Strict equal vs. equal

---
+++ xtraExercises/strictequals/strictequals.js
@@ -0,0 +1,11 @@
+/*
+
+What is the difference between '==' vs. '==='
+
+'==' 
+    tests for abstact qualities
+    does not test for data type
+
+'===' 
+    tests or strict qualities
+    does test for data type
